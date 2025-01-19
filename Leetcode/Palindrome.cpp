e858d64606e075ff92aedf0d76fd25bb40349fd0
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-01-19 23:11:38 +0530

    commit

---
+++ Leetcode/Palindrome.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int reverse = 0;
+        int num = x;
+        while (num > 0) {
+            int d = num % 10;
+            if ((reverse > INT_MAX / 10) || (reverse < INT_MIN / 10)) {
+                return false;
+            }
+            reverse = reverse * 10 + d;
+            num = num / 10;
+        }
+        return reverse == x;
+    }
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << isPalindrome(n) << endl;
+    return 0;
+}
